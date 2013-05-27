deef2eed0fc6057c42725288e786b22c838a8739
robin <mc-robin4002@gmx.fr>
2013-05-27 23:02:13 +0200

    A lot of work, but unfinished

--- src/minecraft/fr/mcnanotech/FantaBobMod/common/FantaBobMod.java
+++ src/minecraft/fr/mcnanotech/FantaBobMod/common/FantaBobMod.java
@@ -1,16 +1,10 @@
 package fr.mcnanotech.FantaBobMod.common;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
 import java.util.Map;
 import java.util.logging.Logger;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.StepSound;
-import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.ModelBiped;
 import net.minecraft.client.renderer.entity.RenderBiped;
 import net.minecraft.creativetab.CreativeTabs;
@@ -39,6 +33,8 @@ import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.EntityRegistry;
+import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
 @Mod(modid = "FantaBobMod", name = "Fanta Bob Mod", version = "2.0.0")
@@ -112,7 +108,7 @@ public class FantaBobMod
 		JeankevinSpawnRate = cfg.get("Mob Spawn Rate", "Jean kevin", 10).getInt();
 		BotlennonSpawnRate = cfg.get("Mob Spawn Rate", "Botlennon", 9).getInt();
 
-		isImmuneToFire = cfg.get("BobLennon", "boblennon.immunetofire", true).getBoolean(true);
+		isImmuneToFire = cfg.get("BobLennon", "Immunetofire", true).getBoolean(true);
 		isPyromaniac = cfg.get("BobLennon", "Pyromaniac", false).getBoolean(true);
 		pyroRate = cfg.get("BobLennon", "Pyromaniac Rate", 40).getInt();
 		canBurnWood = cfg.get("BobLennon", "Fire Wood", true).getBoolean(true);
@@ -125,41 +121,30 @@ public class FantaBobMod
 
 	}
 
-	/*
 	@Init
 	public void load(FMLInitializationEvent event)
 	{
 
 		// enregistrement des mobs
-		ModLoader.registerEntityID(EntityBob.class, "Boblennon", 101, 204, 13613421);
-		ModLoader.registerEntityID(EntityFanta.class, "TheFantasio974", 102, 0, 16777215);
-		ModLoader.registerEntityID(EntityJeanKevin.class, "JeanKevin", 103, 44975, 11762530);
-		ModLoader.registerEntityID(EntityBotlennon.class, "Botlennon", 104, 204, 16777215);
 
-		// localisation des noms des mobs, pour les oeufs
-		LanguageRegistry.instance().addStringLocalization("entity.Boblennon.name", "Boblennon");
-		LanguageRegistry.instance().addStringLocalization("entity.TheFantasio974.name", "TheFantasio974");
-		LanguageRegistry.instance().addStringLocalization("entity.JeanKevin.name", "Jean-Kevin");
-		LanguageRegistry.instance().addStringLocalization("entity.Botlennon.name", "Botlennon");
+		EntityRegistry.registerModEntity(EntityBob.class, "Boblennon", 101, this, 64, 1, true);
+		EntityRegistry.registerModEntity(EntityFanta.class, "TheFantasio974", 102, this, 64, 1, true);
+		EntityRegistry.registerModEntity(EntityJeanKevin.class, "JeanKevin", 103, this, 64, 1, true);
+		EntityRegistry.registerModEntity(EntityBotlennon.class, "Botlennon", 104, this, 64, 1, true);
+		
+		EntityRegistry.registerGlobalEntityID(EntityBob.class, "Boblennon", EntityRegistry.findGlobalUniqueEntityId(), 204, 13613421);
+		EntityRegistry.registerGlobalEntityID(EntityFanta.class, "TheFantasio974", EntityRegistry.findGlobalUniqueEntityId(), 0, 16777215);
+		EntityRegistry.registerGlobalEntityID(EntityJeanKevin.class, "JeanKevin", EntityRegistry.findGlobalUniqueEntityId(), 44975, 11762530);
+		EntityRegistry.registerGlobalEntityID(EntityBotlennon.class, "Botlennon", EntityRegistry.findGlobalUniqueEntityId(), 204, 16777215);
 
-		// fix pour mc 1.3: impossible d'utiliser ModLoader.getUniqueEntityId(),
-		// donc on utilise des ids fixes
-		ModLoader.addEntityTracker(this, EntityBob.class, 101, 64, 1, true);
-		ModLoader.addEntityTracker(this, EntityFanta.class, 102, 64, 1, true);
-		ModLoader.addEntityTracker(this, EntityJeanKevin.class, 103, 64, 1,
-				true);
-		ModLoader.addEntityTracker(this, EntityBotlennon.class, 104, 64, 1,
-				true);
-
-		// paramŽtrage du spawn des mobs
 		if (BoblennonSpawn)
-			ModLoader.addSpawn(EntityBob.class, BoblennonSpawnRate, 1, 2, EnumCreatureType.creature);
+			EntityRegistry.addSpawn(EntityBob.class, BoblennonSpawnRate, 1, 2, EnumCreatureType.creature);
 		if (fantaSpawn)
-			ModLoader.addSpawn(EntityFanta.class, fantaSpawnRate, 1, 2, EnumCreatureType.creature);
+			EntityRegistry.addSpawn(EntityFanta.class, fantaSpawnRate, 1, 2, EnumCreatureType.creature);
 		if (JeankevinSpawn)
-			ModLoader.addSpawn(EntityJeanKevin.class, JeankevinSpawnRate, 2, 3, EnumCreatureType.creature);
+			EntityRegistry.addSpawn(EntityJeanKevin.class, JeankevinSpawnRate, 2, 3, EnumCreatureType.creature);
 		if (BotlennonSpawn)
-			ModLoader.addSpawn(EntityBotlennon.class, BotlennonSpawnRate, 1, 3, EnumCreatureType.monster);
+			EntityRegistry.addSpawn(EntityBotlennon.class, BotlennonSpawnRate, 1, 3, EnumCreatureType.monster);
 
 		// ajout des armures personnalisees
 		ModLoader.addArmor("fantabob");
@@ -167,132 +152,93 @@ public class FantaBobMod
 
 		// instanciation des objets
 		fantaGlasses = new ItemArmor(400, EnumArmorMaterial.CLOTH, 5, 0).setUnlocalizedName("fantaGlasses").setMaxStackSize(64).setIconIndex(ModLoader.addOverride("/gui/items.png", "/fantabob/glasses.png")).setCreativeTab(CreativeTabs.tabDecorations);
-		cobbleTie = (new ItemArmor(401, EnumArmorMaterial.CLOTH, 5, 1))
-				.setItemName("cobbleTie")
-				.setIconIndex(
-						ModLoader.addOverride("/gui/items.png",
-								"/fantabob/cobble_tie.png"))
-				.setCreativeTab(CreativeTabs.tabDecorations);
-		toothBrush = (new ItemObsidianToothBrush(402))
-				.setItemName("toothBrush").setIconIndex(
-						ModLoader.addOverride("/gui/items.png",
-								"/fantabob/obsi_toothbrush.png"));
-		stampCollection = (new Item(403))
-				.setItemName("stampCollection")
-				.setIconIndex(
-						ModLoader.addOverride("/gui/items.png",
-								"/fantabob/stamp_collection.png"))
-				.setCreativeTab(CreativeTabs.tabDecorations);
-		bambooSword = (new ItemSword(404, EnumToolMaterial.WOOD))
-				.setItemName("bambooSword")
-				.setIconIndex(
-						ModLoader.addOverride("/gui/items.png",
-								"/fantabob/bamboo_sword.png"))
-				.setCreativeTab(CreativeTabs.tabCombat);
-		hairPotion = (new ItemArmor(405, EnumArmorMaterial.CLOTH, 6, 0))
-				.setItemName("hairPotion")
-				.setIconIndex(
-						ModLoader.addOverride("/gui/items.png",
-								"/fantabob/hair_potion.png"))
-				.setCreativeTab(CreativeTabs.tabMisc);
-		magabondChop = new ItemFood(406, 4, 0.1F, true)
-				.setPotionEffect(Potion.confusion.id, 20, 0, 0.8F)
-				.setItemName("magabondChop")
-				.setMaxStackSize(64)
-				.setIconIndex(
-						ModLoader.addOverride("/gui/items.png",
-								"/fantabob/magabond_chop.png"))
-				.setCreativeTab(CreativeTabs.tabFood);
-		magabondRecord = (new ItemRecordFantaBob(407, "Magabond", "ATE BITS"))
-				.setIconIndex(
-						ModLoader.addOverride("/gui/items.png",
-								"/fantabob/magabond_record.png")).setItemName(
-						"magabondRecord");
-		bouse = new BlockBouse(200, 0).setHardness(0.6F).setResistance(4.0F)
-				.setBlockName("bouse")
-				.setStepSound(new StepSound("bouse", 3.0F, 1.0F));
-		bouse.blockIndexInTexture = ModLoader.addOverride("/terrain.png",
-				"/fantabob/bouse.png");
+		cobbleTie = new ItemArmor(401, EnumArmorMaterial.CLOTH, 5, 1).setUnlocalizedName("cobbleTie").setIconIndex(ModLoader.addOverride("/gui/items.png","/fantabob/cobble_tie.png")).setCreativeTab(CreativeTabs.tabDecorations);
+		toothBrush = new ItemObsidianToothBrush(402).setUnlocalizedName("toothBrush").setIconIndex(ModLoader.addOverride("/gui/items.png","/fantabob/obsi_toothbrush.png"));
+		stampCollection = new Item(403).setUnlocalizedName("stampCollection").setIconIndex(ModLoader.addOverride("/gui/items.png","/fantabob/stamp_collection.png")).setCreativeTab(CreativeTabs.tabDecorations);
+		bambooSword = new ItemSword(404, EnumToolMaterial.WOOD).setUnlocalizedName("bambooSword").setIconIndex(ModLoader.addOverride("/gui/items.png","/fantabob/bamboo_sword.png")).setCreativeTab(CreativeTabs.tabCombat);
+		hairPotion = new ItemArmor(405, EnumArmorMaterial.CLOTH, 6, 0).setUnlocalizedName("hairPotion").setIconIndex(ModLoader.addOverride("/gui/items.png","/fantabob/hair_potion.png")).setCreativeTab(CreativeTabs.tabMisc);
+		magabondChop = new ItemFood(406, 4, 0.1F, true).setPotionEffect(Potion.confusion.id, 20, 0, 0.8F).setUnlocalizedName("magabondChop").setMaxStackSize(64).setIconIndex(ModLoader.addOverride("/gui/items.png","/fantabob/magabond_chop.png")).setCreativeTab(CreativeTabs.tabFood);
+		magabondRecord = new ItemRecordFantaBob(407, "Magabond", "ATE BITS").setIconIndex(ModLoader.addOverride("/gui/items.png","/fantabob/magabond_record.png")).setUnlocalizedName("magabondRecord");
+		bouse = new BlockBouse(200).setHardness(0.6F).setResistance(4.0F).setUnlocalizedName("bouse").setStepSound(new StepSound("bouse", 3.0F, 1.0F));
+		bouse.blockIndexInTexture = ModLoader.addOverride("/terrain.png","/fantabob/bouse.png");
 
 		// enregistrement des blocs
-		ModLoader.registerBlock(bouse);
+		GameRegistry.registerBlock(bouse, "bouse");
 
 		// recettes de craft
-		ModLoader.addRecipe(new ItemStack(fantaGlasses, 1), new Object[] {
-				"# #", "# #", "X#X", Character.valueOf('#'), Item.stick,
-				Character.valueOf('X'), Block.glass });
-		ModLoader.addRecipe(new ItemStack(cobbleTie, 1),
-				new Object[] { "#", "X", "X", Character.valueOf('#'),
-						Item.silk, Character.valueOf('X'), Block.cobblestone });
-		ModLoader.addRecipe(new ItemStack(toothBrush, 1),
-				new Object[] { "#X", "# ", "# ", Character.valueOf('#'),
-						Block.obsidian, Character.valueOf('X'), Block.cloth });
-		ModLoader.addRecipe(new ItemStack(stampCollection, 1), new Object[] {
-				"#XX", "#XX", "#XX", Character.valueOf('#'), Item.book,
-				Character.valueOf('X'), Item.paper });
-		ModLoader.addRecipe(new ItemStack(bambooSword, 1),
-				new Object[] { "#", "#", "X", Character.valueOf('#'),
-						Item.reed, Character.valueOf('X'), Item.stick });
-		ModLoader.addRecipe(new ItemStack(hairPotion, 1),
-				new Object[] { " # ", "#X#", " # ", Character.valueOf('#'),
-						Block.glass, Character.valueOf('X'), Item.redstone });
-		ModLoader.addRecipe(new ItemStack(bouse, 3), new Object[] { "X", "#",
-				Character.valueOf('#'), Block.dirt, Character.valueOf('X'),
-				Item.bucketWater });
-		ModLoader.addRecipe(new ItemStack(magabondRecord, 1), new Object[] {
-				" # ", "#X#", " # ", Character.valueOf('#'), magabondChop,
-				Character.valueOf('X'), Item.redstone });
+		GameRegistry.addRecipe(new ItemStack(fantaGlasses, 1), new Object[] 
+			{
+				"# #", "# #", "X#X", '#', Item.stick, 'X', Block.glass 
+			});
+		
+		GameRegistry.addRecipe(new ItemStack(cobbleTie, 1), new Object[]
+			{
+				"#", "X", "X", '#', Item.silk, 'X', Block.cobblestone
+			});
 
-		// ajout du nom des blocs/objets
-		ModLoader.addName(fantaGlasses, "Lunettes de Fantasio");
-		ModLoader.addName(cobbleTie, "Cravate en cobble");
-		ModLoader.addName(toothBrush, "Brosse ˆ dents en obsidienne");
-		ModLoader.addName(stampCollection, "Collection de timbres");
-		ModLoader.addName(bambooSword, "EpŽe en bambou");
-		ModLoader.addName(hairPotion, "Lotion capillaire de Papy Lennon");
-		ModLoader.addName(bouse, "Bouse");
-		ModLoader.addName(magabondChop, "C™telette de magabond");
-		ModLoader.addName(magabondRecord, "Disque");
+		GameRegistry.addRecipe(new ItemStack(toothBrush, 1), new Object[] 
+			{ 
+				"#X", "# ", "# ", '#', Block.obsidian, 'X', Block.cloth
+			});
+		
+		GameRegistry.addRecipe(new ItemStack(stampCollection, 1), new Object[] 
+			{
+				"#XX", "#XX", "#XX", '#', Item.book, 'X', Item.paper 
+			});
+		
+		GameRegistry.addRecipe(new ItemStack(bambooSword, 1), new Object[] 
+			{
+				"#", "#", "X", '#', Item.reed, 'X', Item.stick
+			});
+		
+		GameRegistry.addRecipe(new ItemStack(hairPotion, 1), new Object[] 
+			{ 
+				" # ", "#X#", " # ", '#', Block.glass, 'X', Item.redstone
+			});
+		
+		GameRegistry.addRecipe(new ItemStack(bouse, 3), new Object[] 
+			{ 
+				"X", "#", '#', Block.dirt, 'X', Item.bucketWater
+			});
+		
+		GameRegistry.addRecipe(new ItemStack(magabondRecord, 1), new Object[]
+			{
+				" # ", "#X#", " # ", '#', magabondChop, 'X', Item.redstone
+			});
+
+		//Localization 
+		LanguageRegistry.addName(fantaGlasses, "Lunettes de Fantasio");
+		LanguageRegistry.addName(cobbleTie, "Cravate en cobble");
+		LanguageRegistry.addName(toothBrush, "Brosse ˆ dents en obsidienne");
+		LanguageRegistry.addName(stampCollection, "Collection de timbres");
+		LanguageRegistry.addName(bambooSword, "EpŽe en bambou");
+		LanguageRegistry.addName(hairPotion, "Lotion capillaire de Papy Lennon");
+		LanguageRegistry.addName(bouse, "Bouse");
+		LanguageRegistry.addName(magabondChop, "C™telette de magabond");
+		LanguageRegistry.addName(magabondRecord, "Disque");
+		LanguageRegistry.instance().addStringLocalization("entity.Boblennon.name", "Boblennon");
+		LanguageRegistry.instance().addStringLocalization("entity.TheFantasio974.name", "TheFantasio974");
+		LanguageRegistry.instance().addStringLocalization("entity.JeanKevin.name", "Jean-Kevin");
+		LanguageRegistry.instance().addStringLocalization("entity.Botlennon.name", "Botlennon");
 
 		// ajout des achievements
 		// installModAch = new Achievement(887, "installModAch", -2, 0,
 		// Item.paper, null).registerAchievement();
-		killBobAch = new Achievement(888, "killBobAch", -4, 0,
-				Block.cobblestone, AchievementList.openInventory)
-				.registerAchievement();
-		killFantaAch = new Achievement(889, "killFantaAch", -6, 0,
-				Item.swordWood, AchievementList.openInventory)
-				.registerAchievement();
-		floodAch = new Achievement(891, "floodAch", -8, 0, Item.bucketLava,
-				AchievementList.openInventory).registerAchievement();
-		killJeanKevinAch = new Achievement(890, "killJeanKevinAch", -10, 0,
-				Item.arrow, AchievementList.openInventory)
-				.registerAchievement();
-		killBotlennonAch = new Achievement(892, "killBotlennonAch", -12, 0,
-				Item.ingotIron, AchievementList.openInventory)
-				.registerAchievement();
-		getBouseAch = new Achievement(893, "getBouseAch", -14, 0, bouse,
-				AchievementList.openInventory).registerAchievement();
-		brushTeethAch = new Achievement(894, "brushTeethAch", -14, 2,
-				toothBrush, AchievementList.openInventory)
-				.registerAchievement();
-		getHairPotionAch = new Achievement(895, "getHairPotionAch", -12, 2,
-				hairPotion, AchievementList.openInventory)
-				.registerAchievement();
-		getStampCollectionAch = new Achievement(896, "getStampCollectionAch",
-				-10, 2, stampCollection, AchievementList.openInventory)
-				.registerAchievement();
-		getCobbleTieAch = new Achievement(897, "getCobbleTieAch", -8, 2,
-				cobbleTie, AchievementList.openInventory).registerAchievement();
-		getBambooSwordAch = new Achievement(901, "getBambooSwordAch", -6, 2,
-				bambooSword, AchievementList.openInventory)
-				.registerAchievement();
-		getMagabondChopAch = new Achievement(898, "getMagabondChopAch", -8, -2,
-				magabondChop, killJeanKevinAch).registerAchievement();
-		craftMagabondRecAch = new Achievement(899, "craftMagabondRecAch", -10,
-				-2, magabondRecord, getMagabondChopAch).registerAchievement();
-		getFantaGlassesAch = new Achievement(900, "getFantaGlassesAch", -6, -2,
-				fantaGlasses, killFantaAch).registerAchievement();
+		
+		killBobAch = new Achievement(888, "killBobAch", -4, 0, Block.cobblestone, AchievementList.openInventory).registerAchievement();
+		killFantaAch = new Achievement(889, "killFantaAch", -6, 0, Item.swordWood, AchievementList.openInventory).registerAchievement();
+		floodAch = new Achievement(891, "floodAch", -8, 0, Item.bucketLava, AchievementList.openInventory).registerAchievement();
+		killJeanKevinAch = new Achievement(890, "killJeanKevinAch", -10, 0, Item.arrow, AchievementList.openInventory).registerAchievement();
+		killBotlennonAch = new Achievement(892, "killBotlennonAch", -12, 0, Item.ingotIron, AchievementList.openInventory).registerAchievement();
+		getBouseAch = new Achievement(893, "getBouseAch", -14, 0, bouse,AchievementList.openInventory).registerAchievement();
+		brushTeethAch = new Achievement(894, "brushTeethAch", -14, 2,toothBrush, AchievementList.openInventory).registerAchievement();
+		getHairPotionAch = new Achievement(895, "getHairPotionAch", -12, 2,hairPotion, AchievementList.openInventory).registerAchievement();
+		getStampCollectionAch = new Achievement(896, "getStampCollectionAch", -10, 2, stampCollection, AchievementList.openInventory).registerAchievement();
+		getCobbleTieAch = new Achievement(897, "getCobbleTieAch", -8, 2, cobbleTie, AchievementList.openInventory).registerAchievement();
+		getBambooSwordAch = new Achievement(901, "getBambooSwordAch", -6, 2, bambooSword, AchievementList.openInventory).registerAchievement();
+		getMagabondChopAch = new Achievement(898, "getMagabondChopAch", -8, -2, magabondChop, killJeanKevinAch).registerAchievement();
+		craftMagabondRecAch = new Achievement(899, "craftMagabondRecAch", -10, -2, magabondRecord, getMagabondChopAch).registerAchievement();
+		getFantaGlassesAch = new Achievement(900, "getFantaGlassesAch", -6, -2, fantaGlasses, killFantaAch).registerAchievement();
 
 		// descriptions des achievements
 		// ModLoader.addAchievementDesc(installModAch, "Youtuber",
@@ -341,7 +287,6 @@ public class FantaBobMod
 	 * minecraft.thePlayer.triggerAchievement(installModAch); return true; }
 	 */
 
-	/*
 	public void addRenderer(Map map)
 	{
 		map.put(EntityBob.class, new RenderBiped(new ModelBiped(), 0.5F));
@@ -375,5 +320,4 @@ public class FantaBobMod
 		else
 			return null;
 	}
-	*/
 }
