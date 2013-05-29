d004bcb6f3a0e2cdf80ad379cce74e05cf0ca1b4
kevin68 <baumeyerkevin@gmx.fr>
2013-05-29 09:42:10 +0200

    Help for sounds

--- src/minecraft/fr/outadoc/FantaBobMod/client/FTMEventSound.java
+++ src/minecraft/fr/outadoc/FantaBobMod/client/FTMEventSound.java
@@ -1,6 +1,23 @@
 package fr.outadoc.FantaBobMod.client;
 
+import fr.outadoc.FantaBobMod.common.FantaBobMod;
+import net.minecraftforge.client.event.sound.SoundLoadEvent;
+import net.minecraftforge.event.ForgeSubscribe;
+
 public class FTMEventSound 
 {
+    @ForgeSubscribe
+    public void onSound(SoundLoadEvent event)
+    {
+        try
+        {
+            event.manager.soundPoolSounds.addSound("bob1.ogg", FantaBobMod.class.getResource("/mods/FantaBobMod/Sounds/fantabob/bob1.ogg"));
+            
+        }
+        catch (Exception e)
+        {
+            System.err.println("Failed to register one or more sounds.");
+        }
+    }
 
 }
